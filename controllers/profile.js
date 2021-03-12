a58272bbe8d4162840662981fe509c50ab6636a3
German <gerll.bm@hotmail.com>
2021-03-12 00:41:25 -0800

    controllers

---
+++ controllers/profile.js
@@ -0,0 +1,22 @@
+const handleProfile = (req,res)=>{
+    const { id } = req.params;
+    let found = false;
+    db.select('*').from('users').where({id})
+    .then (user => {
+        if (user.length){
+            res.json(user[0])
+        } else{
+            res.status(400).json('Not found')
+        }
+    })
+    .catch(err => res.status(400).json('Error getting user'))
+    /*
+    if (!found){
+        res.status(400).json('not found');
+    }
+    */
+}
+
+module.exports = {
+    handleProfile: handleProfile
+}
---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+//const app = express();
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : 'perros21',
+        database : 'smart-brain'
+    }
+});
+
+
+const app = express();
+app.use (bodyParser.json());
+
+app.use(cors())
+/*
+const database = {
+    users: [
+        {
+            id:'123',
+            name: 'John',
+            password: 'cookies',
+            email: 'john@gmail.com',
+//            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id:'124',
+            name: 'Sally',
+            password: 'bananas',
+            email: 'sally@gmail.com',
+//            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}
+*/
+/*
+app.get('/', (req, res)=>{
+    res.send(database.users);
+})
+*/
+//  signin
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } )
+
+//  register
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+//  Profile/:userId
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+})
+//  Password
+/*
+bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// Load hash from your password DB.
+bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+});
+bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+});
+*/
+app.listen(3000,()=>{
+    console.log('app is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/ signin --> POST = success/fail
+/ register --> POST = user
+/ profile/:userId --> GET = user
+/ image --> PUT --> user
+*/
