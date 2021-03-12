96bfa6cfc92441e2fb9a6785bbdb8c7cab0f6ab2
German <gerll.bm@hotmail.com>
2021-03-12 00:59:17 -0800

    delete some comment test code

--- server.js
+++ server.js
@@ -21,78 +21,18 @@ const db = knex({
     }
 });
 
-
 const app = express();
 app.use (bodyParser.json());
 
 app.use(cors())
-/*
-const database = {
-    users: [
-        {
-            id:'123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-//            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id:'124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-//            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-*/
-/*
-app.get('/', (req, res)=>{
-    res.send(database.users);
-})
-*/
 //  signin
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } )
-
 //  register
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
+// Profile/:userID
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-//  Profile/:userId
+//  Image
 app.put('/image', (req, res) => { image.handleImage (req, res, db)})
-//  Password
-/*
-bcrypt.hash("bacon", null, null, function(err, hash) {
-    // Store hash in your password DB.
-});
-
-// Load hash from your password DB.
-bcrypt.compare("bacon", hash, function(err, res) {
-    // res == true
-});
-bcrypt.compare("veggies", hash, function(err, res) {
-    // res = false
-});
-*/
 app.listen(3000,()=>{
     console.log('app is running on port 3000');
 })
-
-/*
-/ --> res = this is working
-/ signin --> POST = success/fail
-/ register --> POST = user
-/ profile/:userId --> GET = user
-/ image --> PUT --> user
-*/
